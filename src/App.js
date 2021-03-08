b12a30bd2defa9e6f2628b63f321d7b1add5eab5
kiskiliskis <kahlil@kahlilstudios.com>
2021-03-08 23:55:26 +0800

    Typeahed
    search noe working, still needs styling.

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { useState, useEfect } from 'react';
+import { AsyncTypeahead } from 'react-bootstrap-typeahead';
+import './App.css';
+
+
+
+const SEARCH_URI = 'https://api.github.com/search/users';
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [options, setOptions] = useState([]);
+
+  const handleSearch = (query) => {
+    setIsLoading(true);
+
+    fetch(`${SEARCH_URI}?q=${query}+in:login&page=1&per_page=50`)
+      .then((resp) => resp.json())
+      .then(({ items }) => {
+        const options = items.map((i) => ({
+          avatar_url: i.avatar_url,
+          id: i.id,
+          login: i.login,
+        }));
+
+        setOptions(options);
+        setIsLoading(false);
+      });
+  };
+
+  // Bypass client-side filtering by returning `true`. Results are already
+  // filtered by the search endpoint, so no need to do it again.
+  const filterBy = () => true;
+  
+  return (
+    <AsyncTypeahead
+      filterBy={filterBy}
+      id="async-example"
+      isLoading={isLoading}
+      labelKey="login"
+      minLength={3}
+      onSearch={handleSearch}
+      options={options}
+      placeholder="Search for a Github user..."
+      renderMenuItemChildren={(option, props) => (
+        <>
+          <img
+            alt={option.login}
+            src={option.avatar_url}
+            style={{
+              height: '24px',
+              marginRight: '10px',
+              width: '24px',
+            }}
+          />
+          <span>{option.login}</span>
+        </>
+      )}
+    />
+  );
+};
+
+
+export default App;
